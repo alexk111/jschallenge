8872aab7a86fd0e312ff328f4f175e44a24879e3
alexk111 <alexkaul@googlemail.com>
2015-05-16 13:36:15 +0800

    Grunt->Gulp & Improved Structure
    Replaced Grunt with Gulp
    Enabled SASS compiler
    Improved File Structure

--- source/js-challenge-controller.js
+++ source/js-challenge-controller.js
@@ -1,18 +1,9 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name jschallengeApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the jschallengeApp
- */
-angular.module('jschallengeApp')
-
-.controller('MainCtrl', function($scope, $http) {
+jsChallenge.controller('MainCtrl', function($scope, $http) {
 
   // Query for a booking in 1 day from now, for 2 hours.
-  var start = Date.now() + 24 * 3600 * 1000;
+  var start = Date.now() + 2*24 * 3600 * 1000;
   var end = start + 2 * 3600 * 1000;
   var url = 'http://jschallenge.smove.sg/provider/1/availability?book_start=' + start + '&book_end=' + end;
 
---
+++ source/js-challenge.js
@@ -0,0 +1,3 @@
+'use strict';
+
+var jsChallenge = angular.module('jsChallenge', []);
