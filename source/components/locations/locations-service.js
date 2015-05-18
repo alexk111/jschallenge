e48eca2edf909dd2c0dc3a3bc255985dc3f7ea0e
alexk111 <alexkaul@googlemail.com>
2015-05-18 21:52:02 +0800

    Get Locations Data for Time Offsets

--- source/components/locations/locations-service.js
+++ source/components/locations/locations-service.js
@@ -1,27 +1,96 @@
 'use strict';
 
-jsChallenge.service('jscLocationsSrvc', function($http) {
+jsChallenge.service('jscLocationsSrvc', function($http, $q) {
   var that=this,
       locations = [],
       isLoading=false, isError=false;
+
+  function findValueLocByID(values, id) {
+    var value;
+    for(var i=0, len=values.length;i<len;i++) {
+      value=values[i];
+      if(value.id===id) {
+        return value;
+      }
+    }
+  }
+
+  function buildLocation(valueLoc, tsFrom) {
+    locations.push({
+      id: valueLoc.id,
+      code: valueLoc.code,
+      lat: valueLoc.latitude,
+      lng: valueLoc.longitude,
+      name: valueLoc.parking_shortname,
+      tsAvail: tsFrom
+    });
+  }
+
+  function buildLocations(values, tsFrom) {
+    var i,len, valueLoc, valueLocA, id;
+    console.log('Result from the API calls:', values);
+
+    locations=[];
+    for(i=0, len=values[2].data.length;i<len;i++) {
+      valueLoc=values[2].data[i];
+      valueLocA=valueLoc;
+      if(!valueLocA.cars_available) {
+        console.info('(Offset 0) No Avail Cars for: ', valueLoc);
+        id=valueLoc.id;
+        valueLocA=findValueLocByID(values[1].data,id);
+        if(!valueLocA || !valueLocA.cars_available) {
+          console.info('(Offset -15) No Avail Cars for: ', valueLoc);
+          valueLocA=findValueLocByID(values[3].data,id);
+          if(!valueLocA || !valueLocA.cars_available) {
+            console.info('(Offset +15) No Avail Cars for: ', valueLoc);
+            valueLocA=findValueLocByID(values[0].data,id);
+            if(!valueLocA || !valueLocA.cars_available) {
+              console.info('(Offset -30) No Avail Cars for: ', valueLoc);
+              valueLocA=findValueLocByID(values[4].data,id);
+              if(!valueLocA || !valueLocA.cars_available) {
+                console.info('(Offset +30) No Avail Cars for: ', valueLoc);
+                buildLocation(valueLoc, undefined);
+              } else {
+                buildLocation(valueLoc, tsFrom+30*60*1000);
+              }
+            } else {
+              buildLocation(valueLoc, tsFrom-30*60*1000);
+            }
+          } else {
+            buildLocation(valueLoc, tsFrom+15*60*1000);
+          }
+        } else {
+          buildLocation(valueLoc, tsFrom-15*60*1000);
+        }
+      } else {
+        buildLocation(valueLoc, tsFrom);
+      }
+    }
+
+  }
 
   this.reload=function(date, time, duration) {
     isError=false;
     isLoading=true;
 
-    var start = date.getTime() + (time.getHours()*60 + time.getMinutes())*1000;
-    var end = start + duration * 60 * 1000;
-    var url = 'http://jschallenge.smove.sg/provider/1/availability?book_start=' + start + '&book_end=' + end;
+    var tsFrom = date.getTime() + (time.getHours()*60 + time.getMinutes())*1000,
+        tsTo = tsFrom + duration * 60 * 1000,
+        url, i, promises=[];
 
-    $http.get(url).success(function(result) {
-      console.log('Result from the API call:', result);
+    for(i=-30*60*1000;i<=30*60*1000;i+=15*60*1000) {
+      url = 'http://jschallenge.smove.sg/provider/1/availability?book_start=' + (tsFrom+i) + '&book_end=' + (tsTo+i);
+      promises.push($http.get(url));
+    }
+
+    $q.all(promises).then(function(values){
+      buildLocations(values, tsFrom);
       isLoading=false;
-    }).error(function(err) {
-      // Hum, this is odd ... contact us...
+    }, function(err){
       console.error(err);
       isLoading=false;
       isError=true;
     });
+
   };
 
   this.isLoading=function() {
