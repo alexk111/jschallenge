8249fea297bc68641672313bdb6efb569d4cf904
alexk111 <alexkaul@googlemail.com>
2015-05-18 13:20:48 +0800

    Time Picker

--- source/components/time-picker/time-picker-directive.js
+++ source/components/time-picker/time-picker-directive.js
@@ -1,8 +1,33 @@
-jsChallenge.directive('jscTimePicker', function() {
+'use strict';
+
+jsChallenge.directive('jscTimePicker', function($interval) {
   return {
     restrict: 'E',
     templateUrl: 'time-picker/time-picker-directive.html',
+    scope: {
+      model: '=ngModel'
+    },
     link: function(scope, element) {
+      /*
+      On Change
+      If Invalid value, move to the next valid
+      Example: MinTime=8:00; If decrease 8:00, it sets 23:50 (decreases 24:00); If increase 23:50, it sets 8:00
+      */
+
+      function updateLimitDates() {
+        scope.minDate=new Date(Date.now() + 2 * 3600 * 1000);
+      }
+      updateLimitDates();
+
+      var intervalUpdateLimitDates=$interval(function(){
+        updateLimitDates();
+      },15*60000);
+
+      // Cancel Intervals on Destroy
+      scope.$on('$destroy', function() {
+        $interval.cancel( intervalUpdateLimitDates );
+      });
+
     }
   }
 });
