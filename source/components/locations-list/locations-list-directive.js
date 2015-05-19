51f38a87e96fbe2c02cfa2ba165c8fdadbf32739
alexk111 <alexkaul@googlemail.com>
2015-05-19 11:55:35 +0800

    Locations List directive

--- source/components/locations-list/locations-list-directive.js
+++ source/components/locations-list/locations-list-directive.js
@@ -1,10 +1,15 @@
 'use strict';
 
-jsChallenge.directive('jscLocationsList', function() {
+jsChallenge.directive('jscLocationsList', function(jscLocationsSrvc) {
   return {
     restrict: 'E',
     templateUrl: 'locations-list/locations-list-directive.html',
     link: function(scope, element) {
+      scope.getLocations=jscLocationsSrvc.getLocations;
+
+      scope.clickLocation=function(location) {
+        console.info('Location Clicked:', location);
+      };
     }
   }
 });
