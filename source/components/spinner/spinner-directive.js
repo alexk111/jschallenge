3e27acd653da37468ba906914eb5a34947190124
alexk111 <alexkaul@googlemail.com>
2015-05-20 12:43:37 +0800

    Transitions and Animations

--- source/components/spinner/spinner-directive.js
+++ source/components/spinner/spinner-directive.js
@@ -1,9 +1,10 @@
-jsChallenge.directive('jscSpinner', function() {
+jsChallenge.directive('jscSpinner', function($animate) {
   return {
     restrict: 'E',
     replace: true,
     templateUrl: 'spinner/spinner-directive.html',
     link: function(scope, element) {
+      $animate.enabled(false, element);
     }
   }
 });
--- source/js-challenge.js
+++ source/js-challenge.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var jsChallenge = angular.module('jsChallenge', [
+  'ngAnimate',
   'ui.bootstrap',
   'uiGmapgoogle-maps'
-]);
+]).config(function($animateProvider) {
+  $animateProvider.classNameFilter(/^(?:(?!ng-animate-disabled).)*$/);
+});
